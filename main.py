ca0d3dbc3b5f6fe0556cb8bd5e05f79b0bbff85e
keywind127 <watersprayer127@gmail.com>
2024-11-14 20:55:18 +0800

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+
+class Person:
+    def greet(self) -> None:
+        raise NotImplementedError("You have not implemented this function.")
+    
+# kevin 向廷
+class Kevin(Person):
+    def greet(self) -> None:
+        print("Hello, my name is 向廷")
+
+# river 俊穎
+
+# smith 君偉
+
+# david 昕哲
+
+if __name__ == "__main__":
+
+    # kevin 向廷
+    Kevin().greet()
+
+    # river 俊穎
+
+    # smith 君偉
+
+    # david 昕哲
